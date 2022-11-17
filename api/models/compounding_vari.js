49378f4f176643a21bc036da94ce0100352e4bef
yesg98 <61418190+yesg98@users.noreply.github.com>
2022-11-17 15:54:44 +0900

    컴파운딩1데이터베이스 메소드 설정

--- api/models/compounding_vari.js
+++ api/models/compounding_vari.js
@@ -14,5 +14,32 @@ const Compounding1 = new Schema({
     tenStrength:Number
 });
 
+Compounding1.statics.newData = function({polyState,mixRatio,extSpinspeed,extDiameter,extTemp,meltTemp,stikic,tenStrength}){ // 신청 문서 생성
+    const data = new this({polyState,mixRatio,extSpinspeed,extDiameter,extTemp,meltTemp,stikic,tenStrength});
+    return data.save();
+};
+
+Compounding1.statics.findDataByAll = function({meltTemp,stikic,tenStrength}){
+    return this.find({meltTemp,stikic,tenStrength}).exec();
+};
+Compounding1.statics.findDataByM = function({meltTemp}){
+    return this.find({meltTemp}).exec();
+};
+Compounding1.statics.findDataByS = function({stikic}){
+    return this.find({stikic}).exec();
+};
+Compounding1.statics.findDataByT = function({tenStrength}){
+    return this.find({tenStrength}).exec();
+};
+Compounding1.statics.findDataByMS = function({meltTemp,stikic}){
+    return this.find({meltTemp,stikic}).exec();
+};
+Compounding1.statics.findDataByMT = function({meltTemp,tenStrength}){
+    return this.find({meltTemp,tenStrength}).exec();
+};
+Compounding1.statics.findDataByST = function({stikic,tenStrength}){
+    return this.find({stikic,tenStrength}).exec();
+};
+
 // 스키마를 모델로 변환하여, 내보내기 합니다.
 module.exports = mongoose.model('Compounding1', Compounding1);
